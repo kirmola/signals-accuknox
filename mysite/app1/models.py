a8e3a0cfc17165669e39986b43cf892ae5fd1e1a
kirmolaa <kirmolaa@gmail.com>
2024-09-11 10:09:10 +0000

    renamed models. fixed code

--- mysite/app1/models.py
+++ mysite/app1/models.py
@@ -3,5 +3,11 @@ from django.db import models
 # Create your models here.
 
 
-class TestModel(models.Model):
+class TestModel1(models.Model):
+    name = models.CharField(max_length=100)
+
+class TestModel2(models.Model):
+    name = models.CharField(max_length=100)
+
+class TestModel3(models.Model):
     name = models.CharField(max_length=100)
--- mysite/app1/signals.py
+++ mysite/app1/signals.py
@@ -5,19 +5,23 @@ from .models import TestModel
 
 
 # Signal receiver
-@receiver(post_save, sender=TestModel)
+@receiver(post_save, sender=TestModel1)
 def testEntryHandler(sender, instance, **kwargs):
     print("Signal handler -> Signal received, starting task.")
     time.sleep(3)  # Simulate a delay
     print("Signal handler: Task completed after 3 seconds.")
 
 
-@receiver(post_save, sender=TestModel)
+@receiver(post_save, sender=TestModel2)
 def entryThreadID(sender, instance, **kwargs):
     print(f"Signal handler: Running in thread {threading.current_thread().name}, ID: {threading.get_ident()}")
 
 
-@receiver(post_save, sender=TestModel)
+@receiver(post_save, sender=TestModel3)
 def AtomicTxnHandler(sender, instance, **kwargs):
     print("Signal handler: Saving Entry inside signal.")
-    TestModel.objects.create(message=f"{secrets.token_urlsafe(23)}")
+    entry = TestModel3.objects.create(message=f"{secrets.token_urlsafe(23)}")
+    if entry.created:
+        return True
+
+    return False
